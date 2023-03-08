a1e5d66c22f96bc22a87fabb9e8c3bb7e9dfd9e6
georgiagrayland <georgiamayeva@live.co.uk>
2023-03-08 15:22:05 +0000

    add js code to link email js and add error codes

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,14 +1,15 @@
+const contactForm = document.getElementById("contact-me");
+
 function sendMail(contactForm) {
     emailjs.send("gmail", "georgia", {
         "from_name": contactForm.name.value,
-        "from_email": contactForm.email.value,
-        "project_request": contactForm.projectSummary.value
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
     })
     .then(function(response) {
         console.log('SUCCESS!', response.status, response.text);
      }, function(error) {
         console.log('FAILED...', error);
      });
-    return false; //to block from loading a new page  
+    return false;  // To block from loading a new page
 }
-
