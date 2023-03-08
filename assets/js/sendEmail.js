8f01aeefec06ebb05351f39f7df8238e846c8d74
georgiagrayland <georgiamayeva@live.co.uk>
2023-03-08 14:57:57 +0000

    Implement email js integration on the contact page

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,14 @@
-console.log("LOL!");
+function sendMail(contactForm) {
+    emailjs.send("gmail", "georgia", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "project_request": contactForm.projectSummary.value
+    })
+    .then(function(response) {
+        console.log('SUCCESS!', response.status, response.text);
+     }, function(error) {
+        console.log('FAILED...', error);
+     });
+    return false; //to block from loading a new page  
+}
+
